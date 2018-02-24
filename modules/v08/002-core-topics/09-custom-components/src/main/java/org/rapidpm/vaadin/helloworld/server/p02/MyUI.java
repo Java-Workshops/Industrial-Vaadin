0a873fda565b8d325d42d41ef85c7b34ffd95b5c
Sven Ruppert <sven.ruppert@gmail.com>
2018-02-24 07:57:47 +0200

    added custom component demo

--- modules/v08/002-core-topics/09-custom-components/src/main/java/org/rapidpm/vaadin/helloworld/server/p02/MyUI.java
+++ modules/v08/002-core-topics/09-custom-components/src/main/java/org/rapidpm/vaadin/helloworld/server/p02/MyUI.java
@@ -24,61 +24,16 @@ import java.util.function.Supplier;
 
 /**
  * will crash on OSX and OpenJDK8 (9 and 10 is ok) -> Linkage error
+ *
+ * Build a CustomerForm
  */
 @PreserveOnRefresh
 public class MyUI extends CoreUI {
 
-  public static class MyImageSource implements StreamResource.StreamSource, HasLogger {
-
-    public MyImageSource() {
-      logger().warning("MyImageSource - constructor");
-    }
-
-    ByteArrayOutputStream imagebuffer = null;
-    int                   reloads     = 0;
-
-    public InputStream getStream() {
-
-      logger().info("reloads " + reloads);
-      // Create an image
-      BufferedImage image = new BufferedImage(400, 400,
-                                              BufferedImage.TYPE_INT_RGB
-      );
-      Graphics2D drawable = image.createGraphics();
-      // Draw something static
-      drawable.setStroke(new BasicStroke(5));
-      drawable.setColor(Color.WHITE);
-      drawable.fillRect(0, 0, 400, 400);
-      drawable.setColor(Color.BLACK);
-      drawable.drawOval(50, 50, 300, 300);
-      // Draw something dynamic
-      drawable.setFont(new Font(null, Font.PLAIN, 48));
-      drawable.drawString("Reloads=" + reloads, 75, 216);
-      reloads++;
-      drawable.setColor(new Color(0, 165, 235));
-      int x = (int) (200 - 10 + 150 * Math.sin(reloads * 0.3));
-      int y = (int) (200 - 10 + 150 * Math.cos(reloads * 0.3));
-      drawable.fillOval(x, y, 20, 20);
-      try {
-        // Write the image to a buffer
-        imagebuffer = new ByteArrayOutputStream();
-        ImageIO.write(image, "png", imagebuffer);
-        // Return a stream from the buffer
-        return new ByteArrayInputStream(imagebuffer.toByteArray());
-      } catch (IOException e) {
-        return null;
-      }
-    }
-  }
 
   @Override
   public Supplier<Component> componentSupplier() {
-    return () -> {
-      final StreamResource streamResource = new StreamResource(new MyImageSource(), "generated" + System.nanoTime() + ".png");
-      streamResource.setCacheTime(0);
-//      streamResource.setMIMEType();
-      return new Image("Dynamic Pic " + System.nanoTime(), streamResource);
-    };
+    return () -> new Label("not yet implemented...");
   }
 
 
